39167df4727223ad2befaa6836b635ec9c582c6e
Jan Jones <janjones@microsoft.com>
2023-03-24 16:16:54 +0100

    Create MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RazorIssue8502_api.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
